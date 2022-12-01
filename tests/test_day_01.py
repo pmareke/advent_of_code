6cc2241d893cf971a8e8603f1ed8b96b113188f2
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-01 21:39:47 +0100

    feat: remove unused import

--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -4,9 +4,10 @@ from days.day_01 import Day01
 
 
 class TestDay01:
-    def setup(self, lines: list[str]) -> None: 
-        lines = [line.strip() for line in open("inputs/1.in")]
-        self.day = Day01(lines)
+    def setup(self, lines: list[str]) -> None:
+        with open("inputs/1.in") as file:
+            lines = [line.strip() for line in file]
+            self.day = Day01(lines)
 
     def test_part_one(self) -> None:
         result = self.day.part_one()
--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -1,4 +1,3 @@
-import pytest
 from expects import equal, expect
 
 from days.day_02 import Day02
@@ -6,8 +5,9 @@ from days.day_02 import Day02
 
 class TestDay02:
     def setup(self) -> None:
-        lines = [line.strip() for line in open("inputs/2.in")]
-        self.day = Day02(lines)
+        with open("inputs/1.in") as file:
+            lines = [line.strip() for line in file]
+            self.day = Day02(lines)
 
     def test_part_one(self) -> None:
         result = self.day.part_one()
