e7d46516befa773735fa48252b6887d2ceb41429
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-01 21:40:48 +0100

    feat: simplify test day 2

--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -4,7 +4,7 @@ from days.day_01 import Day01
 
 
 class TestDay01:
-    def setup(self, lines: list[str]) -> None:
+    def setup(self) -> None:
         with open("inputs/1.in") as file:
             lines = [line.strip() for line in file]
             self.day = Day01(lines)
