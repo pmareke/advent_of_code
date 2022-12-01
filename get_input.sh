c3ff12f9f82881a0136c33166ca223d23a2a29b5
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-12-01 16:19:46 +0100

    feat: fix get input

--- get_input.sh
+++ get_input.sh
@@ -3,7 +3,7 @@
 get_input () {
   header="Cookie:$ADVENT_OF_CODE_COOKIE"
 
-  curl --header $header https://adventofcode.com/2021/day/$1/input > "inputs/$1.in"
+  curl --header $header https://adventofcode.com/2022/day/$1/input > "inputs/$1.in"
 }
 
 get_input $1
