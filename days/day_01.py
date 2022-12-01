c9d19173a15087b4d2c3bfb02f5760ff7759c8be
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-12-01 08:44:42 +0100

    feat: solved day one

---
+++ days/day_01.py
@@ -0,0 +1,28 @@
+import pytest
+from dataclasses import dataclass
+from expects import equal, expect
+from typing import List
+
+@dataclass
+class Day01:
+    lines: List[str]
+
+    def part_one(self) -> int:
+        return max(self._calculate_calories())
+
+    def part_two(self) -> int:
+        sorted_calories = sorted(self._calculate_calories(), reverse=True)
+        print(sorted_calories)
+        return sum(sorted_calories[:3])
+
+    def _calculate_calories(self) -> List[int]:
+        calories: List[int] = []
+        acc = 0
+        for line in self.lines:
+            if line == "\n":
+                calories.append(acc)
+                acc = 0
+                continue
+            acc += int(line)
+        return calories
+
---
+++ tests/test_day_01.py
@@ -0,0 +1,26 @@
+import pytest
+from expects import equal, expect
+from typing import List
+
+from days.day_01 import Day01
+
+class TestDay01:
+    @pytest.fixture
+    def lines(self) -> List[str]:
+        return [line for line in open("inputs/1.in")]
+
+    def test_part_one(self, lines: List[str]) -> None:
+        day = Day01(lines)
+        
+        result = day.part_one()
+
+        expect(result).to(equal(72070))
+
+    def test_part_two(self, lines: List[str]) -> None:
+        day = Day01(lines)
+        
+        result = day.part_two()
+
+        expect(result).to(equal(211805))
+       
+
