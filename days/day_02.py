dd714035cede38f07dd0f0e2ec1e16011ca36a8d
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-02 18:05:27 +0100

    feat: refactor day two

--- days/day_02.py
+++ days/day_02.py
@@ -13,50 +13,38 @@ class Day02:
             score += self._play_round(oponent, myself)
         return score
 
+    def part_two(self) -> int:
+        score = 0
+        for line in self.lines:
+            oponent, myself = self._read_moves(line)
+            score += self._play_round_with_winner(oponent, myself)
+        return score
+
     def _play_round(self, oponent: str, myself: str) -> int:
-        points_per_hand_shape = {"X": 1, "Y": 2, "Z": 3}[myself]
-        points_per_both_hand_shapes = {
+        points_map = {
             "A": {"X": 3, "Y": 6, "Z": 0},
             "B": {"X": 0, "Y": 3, "Z": 6},
             "C": {"X": 6, "Y": 0, "Z": 3},
         }
-        hand_score = points_per_both_hand_shapes[oponent][myself]
-        return hand_score + points_per_hand_shape
+        hand_score = points_map[oponent][myself]
+        extra_points = {"X": 1, "Y": 2, "Z": 3}
+        return hand_score + extra_points[myself]
 
-    def part_two(self) -> int:
-        score = 0
-        for line in self.lines:
-            oponent, result = self._read_moves(line)
-            score += self._play_round_with_winner(oponent, result)
-        return score
-
-    def _play_round_with_winner(self, oponent: str, result: str) -> int:
-        points_per_hand_shape = {"A": 1, "B": 2, "C": 3}
-        points_per_both_hand_shapes = {
-            "Y": {
-                "A": 3 + points_per_hand_shape[oponent],
-                "B": 3 + points_per_hand_shape[oponent],
-                "C": 3 + points_per_hand_shape[oponent],
-            },
-            "Z": {
-                "A": 6 + points_per_hand_shape["B"],
-                "B": 6 + points_per_hand_shape["C"],
-                "C": 6 + points_per_hand_shape["A"],
-            },
-            "X": {
-                "A": 0 + points_per_hand_shape["C"],
-                "B": 0 + points_per_hand_shape["A"],
-                "C": 0 + points_per_hand_shape["B"],
-            },
+    def _play_round_with_winner(self, oponent: str, myself: str) -> int:
+        points_map = {
+            "Y": {"A": 1, "B": 2, "C": 3},
+            "Z": {"A": 2, "B": 3, "C": 1},
+            "X": {"A": 3, "B": 1, "C": 2},
         }
-        return points_per_both_hand_shapes[result][oponent]
+        hand_score = points_map[myself][oponent]
+        extra_points = {"X": 0, "Y": 3, "Z": 6}
+        return hand_score + extra_points[myself]
 
     @staticmethod
     def _read_moves(line: str) -> tuple[str, str]:
         move_regex = re.compile(r"(?P<oponent>[ABC]) (?P<myself>[XYZ])")
         matches = move_regex.search(line)
-        if not matches:
-            raise Exception
+        assert matches
         oponent = matches.group("oponent")
         myself = matches.group("myself")
         return (oponent, myself)
