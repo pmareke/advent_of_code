64a5859120a80633abe6cfca3dcddd08d5aafc7d
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-03 18:31:34 +0100

    feat: refactor day one

--- days/day_01.py
+++ days/day_01.py
@@ -6,15 +6,14 @@ class Day01:
     lines: list[str]
 
     def part_one(self) -> int:
-        return max(self._calculate_calories())
+        return self._calculate_sorted_calories()[0]
 
     def part_two(self) -> int:
-        sorted_calories = sorted(self._calculate_calories(), reverse=True)
-        return sum(sorted_calories[:3])
+        return sum(self._calculate_sorted_calories()[:3])
 
-    def _calculate_calories(self) -> list[int]:
+    def _calculate_sorted_calories(self) -> list[int]:
         calories: list[int] = []
         for elf in "\n".join(self.lines).split("\n\n"):
             more_calories = sum(int(line) for line in elf.split("\n"))
             calories.append(more_calories)
-        return calories
+        return sorted(calories, reverse=True)
