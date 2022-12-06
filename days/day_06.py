09adef33a57eaeaed246dc60469d5329340e0c77
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-06 08:30:58 +0100

    feat: refactor day six

--- days/day_06.py
+++ days/day_06.py
@@ -13,8 +13,8 @@ class Day06:
 
     def _find_start_of_packet(self, part_two: bool = False) -> int:
         number_of_characters = 14 if part_two else 4
-        for index, _ in enumerate(self.line):
-            if index + number_of_characters <= len(self.line):
+        for index in range(len(self.line)):
+            if index - number_of_characters <= len(self.line):
                 block = self.line[index : index + number_of_characters]
                 if len(set(block)) == number_of_characters:
                     return index + number_of_characters
