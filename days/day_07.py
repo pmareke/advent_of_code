3afadf32601a0301185d5bbe7dc1be157a070f5e
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-07 22:16:42 +0100

    feat: refactor day seven

--- days/day_07.py
+++ days/day_07.py
@@ -1,97 +1,37 @@
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Optional
 
 
 @dataclass
-class File:
-    size: int
-    name: str
-
-
-class Directory:
-    def __init__(self, name: str, parent: Optional["Directory"] = None) -> None:
-        self.name = name
-        self.parent = parent
-        self.files: list[File] = []
-        self.directories: dict[str, "Directory"] = defaultdict()
-
-    @property
-    def size(self) -> int:
-        file_size = sum(file.size for file in self.files)
-        sizes = [child.size for child in self.directories.values()]
-        return file_size + sum(sizes)
-
-
-LIMIT_DISK_SPACE = 70000000
-UPDATE_MINIMUM_SPACE = 30000000
-
-
 class Day07:
-    def __init__(self, lines: list[str]) -> None:
-        self.lines = lines
-        self.sizes: list[list[int]] = []
+    lines: list[str]
 
     def part_one(self) -> int:
-        root = self._build_tree()
-        self._calculate_sizes(root)
-        result = []
-        for size in self.sizes:
-            result.extend([sum(s for s in size if s <= 100000)])
-        return sum(result)
+        tree = self._build_tree()
+        return sum(size for size in tree.values() if size < 100000)
 
     def part_two(self) -> int:
-        root = self._build_tree()
-        self._calculate_sizes(root)
-        unused_space = LIMIT_DISK_SPACE - root.size
-        needed_space = UPDATE_MINIMUM_SPACE - unused_space
-        result = []
-        for sizes in self.sizes:
-            result.extend([size for size in sizes if size > needed_space])
-        return int(sorted(result)[0])
+        limit_disk_space = 70000000
+        update_minimum_space = 30000000
+        tree = self._build_tree()
+        needed_space = tree["/"] - (limit_disk_space - update_minimum_space)
+        valid_paths = [path for path in tree.values() if path >= needed_space]
+        return sorted(valid_paths)[0]
 
-    def _build_tree(self) -> Directory:
-        root = Directory(name="/")
-        current_directory = root
-        for line in self.lines[1:]:
-            assert current_directory
-            if line == "$ ls":
+    def _build_tree(self) -> dict[str, int]:
+        tree: dict[str, int] = defaultdict(int)
+        stack: list[str] = []
+        for line in self.lines:
+            if line == "$ ls" or line.startswith("dir"):
                 continue
             if line == "$ cd ..":
-                assert current_directory.parent
-                current_directory = current_directory.parent
+                stack.pop()
                 continue
             if line.startswith("$ cd "):
-                name = line.split("$ cd ")[1]
-                current_directory = current_directory.directories[name]
+                path = line.split("$ cd ")[1]
+                stack.append(path)
                 continue
-            if line.startswith("dir "):
-                new_dir = self._create_directory_from_line(line, current_directory)
-                current_directory.directories[new_dir.name] = new_dir
-                continue
-            current_directory.files.append(self._create_file_from_line(line))
-        return root
-
-    @staticmethod
-    def _create_directory_from_line(
-        line: str, current_directory: Directory
-    ) -> Directory:
-        name = line.split("dir ")[1]
-        return Directory(name=name, parent=current_directory)
-
-    @staticmethod
-    def _create_file_from_line(line: str) -> File:
-        size, name = line.split(" ")
-        return File(name=name, size=int(size))
-
-    def _calculate_sizes(self, root: Directory) -> None:
-        for child in root.directories.values():
-            self._walk(child, [root.size, child.size])
-
-    def _walk(self, directory: Directory, acc: list[int]) -> list[int]:
-        if not directory.directories:
-            self.sizes.append(acc)
-            return acc
-        for child in directory.directories.values():
-            self._walk(child, [*acc, child.size])
-        return acc
+            size = line.split(" ")[0]
+            for i in range(1, len(stack) + 1):
+                tree["/".join(stack[:i])] += int(size)
+        return tree
