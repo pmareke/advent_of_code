8f6ce1843165ddd34d92959348d1993b098925a1
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-07 20:47:06 +0100

    feat: refactor day seven (WIP)

--- days/day_07.py
+++ days/day_07.py
@@ -26,63 +26,49 @@ class Directory:
         return f"name={self.name}, directories={self.directories}"
 
 
-@dataclass
 class Day07:
-    lines: list[str]
-    total = []
-    total2 = []
+    def __init__(self, lines: list[str]) -> None:
+        self.lines = lines
+        self.total: list[int] = []
+        self.total2: list[list[int]] = []
 
     def part_one(self) -> int:
-        root = Directory(name="/")
-        current_directory = root
-        for line in self.lines[1:]:
-            assert current_directory
-            if line == "$ ls":
-                continue
-            if line == "$ cd ..":
-                current_directory = current_directory.parent
-                continue
-            if line.startswith("$ cd "):
-                name = line.split("$ cd ")[1]
-                current_directory = current_directory.directories[name]
-                continue
-            if line.startswith("dir "):
-                new_dir = self._create_directory_from_line(line)
-                new_dir.parent = current_directory
-                current_directory.directories[new_dir.name] = new_dir
-                continue
-            current_directory.files.append(self._create_file_from_line(line))
-        return sum(self._calculate_sizes(root))
+        root = self._build_tree()
+        self._calculate_sizes(root)
+        return sum(self.total)
 
     def part_two(self) -> int:
-        root = Directory(name="/")
-        current_directory = root
-        for line in self.lines[1:]:
-            assert current_directory
-            if line == "$ ls":
-                continue
-            if line == "$ cd ..":
-                current_directory = current_directory.parent
-                continue
-            if line.startswith("$ cd "):
-                name = line.split("$ cd ")[1]
-                current_directory = current_directory.directories[name]
-                continue
-            if line.startswith("dir "):
-                new_dir = self._create_directory_from_line(line)
-                new_dir.parent = current_directory
-                current_directory.directories[new_dir.name] = new_dir
-                continue
-            current_directory.files.append(self._create_file_from_line(line))
+        root = self._build_tree()
         self._calculate_sizes(root)
         unused_space = 70000000 - root.size
-        print(f"Unused space: {unused_space}")
         needed_space = 30000000 - unused_space
-        print(f"Needed space: {needed_space}")
         result = []
         for sizes in self.total2:
             result.extend([size for size in sizes if size > needed_space])
-        return sorted(result)[0]
+        return int(sorted(result)[0])
+
+    def _build_tree(self) -> Directory:
+        root = Directory(name="/")
+        current_directory = root
+        for line in self.lines[1:]:
+            assert current_directory
+            if line == "$ ls":
+                continue
+            if line == "$ cd ..":
+                assert current_directory.parent
+                current_directory = current_directory.parent
+                continue
+            if line.startswith("$ cd "):
+                name = line.split("$ cd ")[1]
+                current_directory = current_directory.directories[name]
+                continue
+            if line.startswith("dir "):
+                new_dir = self._create_directory_from_line(line)
+                new_dir.parent = current_directory
+                current_directory.directories[new_dir.name] = new_dir
+                continue
+            current_directory.files.append(self._create_file_from_line(line))
+        return root
 
     @staticmethod
     def _create_file_from_line(line: str) -> File:
@@ -94,17 +80,10 @@ class Day07:
         name = line.split("dir ")[1]
         return Directory(name)
 
-    def _calculate_sizes(self, root: Directory) -> list[int]:
+    def _calculate_sizes(self, root: Directory) -> None:
         sizes = [root.size]
         for child in root.directories.values():
             self._walk(child, [*sizes, child.size])
-        return self.total
-
-    def _calculate_sizes2(self, root: Directory) -> list[int]:
-        sizes = [root.size]
-        for child in root.directories.values():
-            self._walk(child, [*sizes, child.size])
-        return self.total2
 
     def _walk(self, directory: Directory, acc: list[int]) -> list[int]:
         if not directory.directories:
