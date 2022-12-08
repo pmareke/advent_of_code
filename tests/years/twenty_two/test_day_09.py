5f5d84d3c1e85c34a3c921c1602762e150441228
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-08 17:34:23 +0100

    feat: create day nine

---
+++ tests/years/twenty_two/test_day_09.py
@@ -0,0 +1,20 @@
+from expects import equal, expect
+
+from years.twenty_two.days.day_09 import Day09
+
+
+class TestDay08:
+    def setup(self) -> None:
+        with open("years/twenty_two/inputs/9.in") as file:
+            lines = [line.strip() for line in file]
+            self.day = Day09(lines)
+
+    def test_part_one(self) -> None:
+        result = self.day.part_one()
+
+        expect(result).to(equal(0))
+
+    def test_part_two(self) -> None:
+        result = self.day.part_two()
+
+        expect(result).to(equal(0))
