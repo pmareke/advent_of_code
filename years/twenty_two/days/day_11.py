8f9c1dca6b8a20cb1624d8c696565fba6100a946
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-11 09:38:40 +0100

    feat: refactor day eleven

--- years/twenty_two/days/day_11.py
+++ years/twenty_two/days/day_11.py
@@ -7,13 +7,9 @@ class Monkey:
     items: list[int]
     operation: Callable
     divisible_by: int
-    test: Callable
     true_monkey: int
     false_monkey: int
     inspected_items: int = 0
-
-    def add_item(self, item: int) -> None:
-        self.items.append(item)
 
 
 @dataclass
@@ -22,46 +18,11 @@ class Day11:
 
     def part_one(self) -> int:
         monkeys = self._create_monkeys()
-        iteration = 0
-        while iteration < 20:
-            iteration += 1
-            for monkey in monkeys:
-                for item in monkey.items:
-                    monkey.inspected_items += 1
-                    new_worry_level = monkey.operation(item) // 3
-                    destination = (
-                        monkey.true_monkey
-                        if monkey.test(new_worry_level)
-                        else monkey.false_monkey
-                    )
-                    monkeys[destination].add_item(new_worry_level)
-                monkey.items = []
-        inspected_items = sorted(
-            [monkey.inspected_items for monkey in monkeys], reverse=True
-        )
-        return inspected_items[0] * inspected_items[1]
+        return self._play_rounds(monkeys, rounds=20)
 
     def part_two(self) -> int:
         monkeys = self._create_monkeys()
-        iteration = 0
-        less_common_divisor = self._find_less_common_divisor(monkeys)
-        while iteration < 10000:
-            iteration += 1
-            for monkey in monkeys:
-                for item in monkey.items:
-                    monkey.inspected_items += 1
-                    new_worry_level = monkey.operation(item) % less_common_divisor
-                    destination = (
-                        monkey.true_monkey
-                        if monkey.test(new_worry_level)
-                        else monkey.false_monkey
-                    )
-                    monkeys[destination].add_item(new_worry_level)
-                monkey.items = []
-        inspected_items = sorted(
-            [monkey.inspected_items for monkey in monkeys], reverse=True
-        )
-        return inspected_items[0] * inspected_items[1]
+        return self._play_rounds(monkeys, rounds=10000)
 
     def _create_monkeys(self) -> list[Monkey]:
         monkeys: list[Monkey] = []
@@ -78,17 +39,40 @@ class Day11:
         callback = lines[2].split("Operation: new = ")[1]
         operation = lambda old: eval(callback)
         divisible_by = lines[3].split("Test: divisible by ")[1]
-        test = lambda old: old % int(divisible_by) == 0
         true_monkey = lines[4].split("If true: throw to monkey ")[1]
         false_monkey = lines[5].split("If false: throw to monkey ")[1]
         return Monkey(
             items,
             operation,
             int(divisible_by),
-            test,
             int(true_monkey),
             int(false_monkey),
         )
+
+    def _play_rounds(self, monkeys: list[Monkey], rounds: int) -> int:
+        iteration = 0
+        while iteration < rounds:
+            iteration += 1
+            self._play_round(monkeys, bool(rounds == 20))
+        inspected_items = sorted([monkey.inspected_items for monkey in monkeys])
+        return inspected_items[-1] * inspected_items[-2]
+
+    def _play_round(self, monkeys: list[Monkey], part_two: bool) -> None:
+        less_common_divisor = self._find_less_common_divisor(monkeys)
+        for monkey in monkeys:
+            for item in monkey.items:
+                monkey.inspected_items += 1
+                if part_two:
+                    new_worry_level = monkey.operation(item) // 3
+                else:
+                    new_worry_level = monkey.operation(item) % less_common_divisor
+                destination = (
+                    monkey.true_monkey
+                    if new_worry_level % monkey.divisible_by == 0
+                    else monkey.false_monkey
+                )
+                monkeys[destination].items.append(new_worry_level)
+            monkey.items = []
 
     @staticmethod
     def _find_less_common_divisor(monkeys: list[Monkey]) -> int:
