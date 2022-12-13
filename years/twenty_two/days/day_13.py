71196ee1684762bb3c10ca239756298167a2a9f3
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-12-13 16:18:47 +0100

    feat: refactor  day 13

--- years/twenty_two/days/day_13.py
+++ years/twenty_two/days/day_13.py
@@ -14,7 +14,7 @@ class Day13:
         for index, packet in enumerate(packets):
             left = eval(packet[0])
             right = eval(packet[1])
-            if self._is_valid(left, right) == -1:
+            if self._compare(left, right) == -1:
                 valid += index + 1
         return valid
 
@@ -26,26 +26,26 @@ class Day13:
             packets.append(eval(left))
             packets.append(eval(right))
         packets.extend([[[2]], [[6]]])
-        sorted_packets = sorted(packets, key=cmp_to_key(self._is_valid))
+        sorted_packets = sorted(packets, key=cmp_to_key(self._compare))
         result = 1
         for index, packet in enumerate(sorted_packets):
             if packet in [[[2]], [[6]]]:
                 result *= index + 1
         return result
 
-    def _is_valid(self, left: Union[list, int], right: Union[list, int]) -> int:
+    def _compare(self, left: Union[list, int], right: Union[list, int]) -> int:
         if isinstance(left, int) and isinstance(right, int):
             if left == right:
                 return 0
             return -1 if left < right else 1
         if isinstance(left, int) and isinstance(right, list):
-            return self._is_valid([left], right)
+            return self._compare([left], right)
         if isinstance(left, list) and isinstance(right, int):
-            return self._is_valid(left, [right])
+            return self._compare(left, [right])
         if isinstance(left, list) and isinstance(right, list):
             i = 0
             while i < len(left) and i < len(right):
-                is_valid = self._is_valid(left[i], right[i])
+                is_valid = self._compare(left[i], right[i])
                 if is_valid != 0:
                     return is_valid
                 i += 1
