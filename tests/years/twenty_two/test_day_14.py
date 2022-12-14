a3fd3909264e2a844099f281988d6e6825c50855
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-12-14 19:05:34 +0100

    feat: mark slow test as only ci

--- tests/years/twenty_two/test_day_14.py
+++ tests/years/twenty_two/test_day_14.py
@@ -1,8 +1,10 @@
+import pytest
 from expects import equal, expect
 
 from years.twenty_two.days.day_14 import Day14
 
 
+@pytest.mark.only_ci
 class TestDay14:
     def setup(self) -> None:
         with open("years/twenty_two/inputs/14.in") as file:
