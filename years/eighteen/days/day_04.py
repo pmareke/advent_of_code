120353db5a83e926df1dac4303bf0a52c93b44a4
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-12-15 19:01:18 +0100

    feat: day 4 2018 (WIP)

--- years/eighteen/days/day_04.py
+++ years/eighteen/days/day_04.py
@@ -3,13 +3,36 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Time:
+    year: int
+    month: int
+    day: int
+    hour: int
+    minute: int
+
+
+@dataclass
+class Guard:
+    guard_id: int
+    slots_sleeps:list[Time] = []
+    slots_awakes: list[Time] = []
+
+    def sleep(self, time: Time) -> None:
+        self.slots_sleeps.append(time)
+
+    def awake(self, time: Time) -> None:
+        self.slots_awakes.append(time)
+
+@dataclass
 class Day04:
     lines: list[str]
 
     def part_one(self) -> int:
+        guards: dict[int, Guard] = {}         
         for line in self.lines:
             entry = self._parse(line)
-            print(entry)
+            guard = self._create_guard_from(entry)
+            guards[guard.guard_id]= guard
         return 0
 
     def part_two(self) -> int:
@@ -18,7 +41,7 @@ class Day04:
     @staticmethod
     def _parse(line: str) -> tuple:
         regex = re.compile(
-            r"\[(?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+) (?P<hour>\d+):(?P<minute>\d+)\] (?P<guard>Guard #(\d+) (.*)|(.*))"
+            r"\[ (?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+) (?P<hour>\d+):(?P<minute>\d+)\] (?P<guard>Guard #(\d+) (.*)|(.*))"
         )
         matches = regex.search(line)
         assert matches
@@ -29,3 +52,7 @@ class Day04:
         minute = int(matches.group("minute"))
         guard = matches.group("guard")
         return (year, month, day, hour, minute, guard)
+
+    @staticmethod
+    def _create_guard_from(entry: tuple) -> Guard:
+        pass
